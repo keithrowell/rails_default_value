bae0054c2f4411978193479988cb10deb3deb068
Keith Rowell <keith@keithrowell.com>
2016-03-24 13:00:30 +1000

    removed debug code

--- lib/rails_default_value/rails_default_value.rb
+++ lib/rails_default_value/rails_default_value.rb
@@ -1,5 +1,5 @@
 module RailsDefaultValue
-  
+
   module InstanceMethods
     
     def set_defaults
@@ -10,15 +10,11 @@ module RailsDefaultValue
           self.send("#{key.to_s}=", value) if (self.send(key.to_s).respond_to?(:nil?) and self.send(key.to_s).nil?) or (self.send(key.to_s).respond_to?(:empty?) and self.send(key.to_s).empty?)
         end
       end
-    
     end
-        
+    
   end
   
   module ClassMethods
-    
-    # class_eval "@@default_values = {}"
-    # class_eval "def default_values ; @@default_values ; end"
     @@default_values = {}
     def default_values ; @@default_values ; end
     
@@ -29,7 +25,6 @@ module RailsDefaultValue
       include InstanceMethods
       before_validation :set_defaults, :on => :create
     end
-      
   end
+  
 end
-
